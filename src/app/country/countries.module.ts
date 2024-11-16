8ea8fa2a24803ccf0be31b266290167632367798
Jhosef <jmperezperez25@gmail.com>
2024-11-16 05:44:06 -0500

    creating forms control

--- src/app/country/countries.module.ts
+++ src/app/country/countries.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { countryRoutingModule } from './country-router.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ SelectorPageComponent ],
   imports: [
     CommonModule ,
     countryRoutingModule ,
+    ReactiveFormsModule ,
   ]
 })
 export class CountriesModule { }
--- src/app/country/pages/selector-page/selector-page.component.ts
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -1,9 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
  
 })
-export class SelectorPageComponent { }
+export class SelectorPageComponent { 
+
+  public myForm: FormGroup
+
+  constructor( private fb:FormBuilder){
+
+    this.myForm = this.fb.group({
+  
+      region: ['',[Validators.required]],
+      country: ['',[Validators.required]],
+      border: ['',[Validators.required]],
+   
+    }
+    
+    )
+  }
+
+
+}
